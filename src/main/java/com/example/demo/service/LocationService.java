d7199ec86e97a466eb20d52d6625ce0d53852fae
bala-amypo <bala@amypo.in>
2025-12-15 16:42:08 +0000

    User commit

--- src/main/java/com/example/demo/service/LocationService.java
+++ src/main/java/com/example/demo/service/LocationService.java
@@ -1,5 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Location;
+
 public interface LocationService {
-    
+
+    Location createLocation(Location location);
+
+    List<Location> getAllLocations();
 }
--- src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.demo.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.service.LocationService;
 
+@Service
 public class LocationServiceImpl implements LocationService{
     
 }
