ea6027b4c6fe75a718cbc3f352ee166199a8b319
bala-amypo <bala@amypo.in>
2025-12-17 10:07:34 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/studentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/studentServiceImpl.java
@@ -1,12 +1,14 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.service.*;
+import com.example.demo.repository.*;
 
 
 @Service
 public class studentServiceImpl implements studentService{
     
     @Autowired
-    
+    studentRepo repo;
+
 
 }
