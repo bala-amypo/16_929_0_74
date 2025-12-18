aca9aca5fcd24b6ee63820c63baca0237e889ba8
bala-amypo <bala@amypo.in>
2025-12-18 13:54:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.repository.repo;
+
 @RestController
 public class Controller {
+
+    @Autowired
+    repo r;
     
     @PostMapping("/add")
-    public User add(@RequestBody )
+    public User add(@RequestBody User user){
+        return r.save(user);
+    }
 }
